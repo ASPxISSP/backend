ef11f82f1645fedc0c7100db0db6c99eee3a24fb
Tymon Śliwiński <tymon.sliwinski24@gmail.com>
2023-11-09 16:10:16 +0100

    feat(app): update app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+
+const configService = new ConfigService();
+const PORT = configService.get('PORT') || 5000;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 }
 bootstrap();
